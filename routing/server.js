0caa520698a5f122cb76e66930ad4efb7fb78de7
Andrew <andrewdebyl@gmail.com>
2024-06-18 18:23:13 -0400

    routing

---
+++ routing/server.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const app = express();
+const path = require('path')
+
+const {logger} = require('./middleware/logEvents')
+const errorHandler = require('./middleware/errorHandler')
+
+const cors = require('cors')
+
+const PORT = process.env.PORT || 3500;
+
+//custom middleware logger
+app.use(logger)
+
+//cross origin resource sharing
+const whitelist = ['https://www.google.com', 'https://127.0.0.1:5500', 'https://localhost:3500']
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    },
+    optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions))
+
+//built in middleware:
+app.use(express.urlencoded({extended: false}))
+
+//built in middleware for json
+app.use(express.json())
+
+//serve static files
+app.use('/', express.static(path.join(__dirname, '/public')))
+app.use('/subdir', express.static(path.join(__dirname, '/public')))
+
+// routes
+app.use('/', require('./routes/root'))
+app.use('/subdir', require('./routes/subdir'))
+app.use('/employees', require('./routes/api/employees'))
+
+
+app.all('*', (req, res) => {
+    res.status(404)
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+    } else if (req.accepts('json')) {
+        res.json({error: "404 not found"})
+    } else {
+        res.type('txt').send("404 not found")
+    }
+})
+
+app.use(errorHandler)
+
+app.listen(PORT, () => console.log(`server run on port ${PORT}`))
